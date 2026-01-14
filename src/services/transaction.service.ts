3224ba525a29ac2e38f93ba867a453a400abd018
pjs1710 <timer973@naver.com>
2026-01-14 11:46:07 +0900

    feat: 거래 내역 필터링 조회(유형별/날짜별)

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,8 +1,8 @@
-import { api } from '../lib/axios';
-import { TransactionResponse, PageResponse } from '../types/api.types';
+import { api } from "../lib/axios";
+import { TransactionResponse, PageResponse } from "../types/api.types";
 
 export const transactionService = {
-  // 거래 내역 조회
+  // 거래 내역 필터링 조회
   getTransactions: async (params: {
     type?: string;
     startDate?: string;
@@ -10,21 +10,28 @@ export const transactionService = {
     page?: number;
     size?: number;
   }): Promise<PageResponse<TransactionResponse>> => {
-    const response = await api.get<PageResponse<TransactionResponse>>('/transactions', {
-      params: {
-        type: params.type,
-        startDate: params.startDate,
-        endDate: params.endDate,
-        page: params.page || 0,
-        size: params.size || 20,
-      },
-    });
+    const response = await api.get<PageResponse<TransactionResponse>>(
+      "/transactions/filter",
+      {
+        params: {
+          type: params.type,
+          startDate: params.startDate,
+          endDate: params.endDate,
+          page: params.page || 0,
+          size: params.size || 20,
+        },
+      }
+    );
     return response.data;
   },
 
   // 거래 상세 조회
-  getTransactionDetail: async (transactionId: number): Promise<TransactionResponse> => {
-    const response = await api.get<TransactionResponse>(`/transactions/${transactionId}`);
+  getTransactionDetail: async (
+    transactionId: number
+  ): Promise<TransactionResponse> => {
+    const response = await api.get<TransactionResponse>(
+      `/transactions/${transactionId}`
+    );
     return response.data;
   },
 };
